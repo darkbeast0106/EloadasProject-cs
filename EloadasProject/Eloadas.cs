9f21991f2779d286c7599878c86471a4a845d7fb
Budaházi Bence <budahazi.bence@petrik.hu>
2022-05-11 14:29:17 +0200

    Add project files.

---
+++ EloadasProject/Eloadas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EloadasProject
+{
+    public class Eloadas
+    {
+        private bool[,] foglalasok;
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            this.foglalasok = new bool[sorokSzama, helyekSzama];
+        }
+    }
+}
